05225d867c27f67ef7aa201f4da4fdd08bf7227e
Robsondj <robson.djsilva@gmail.com>
2022-05-25 14:06:43 -0300

    🚧 adjusted footer component

--- src/components/Footer.test.tsx
+++ src/components/Footer.test.tsx
@@ -8,40 +8,40 @@ jest.mock('../state/hook/useListOfParticipants', () => {
     return {
         useListOfParticipants: jest.fn()
     }
-})
+});
 
-const mockNavegacao = jest.fn();
+const mockNavigation = jest.fn();
 
 jest.mock('react-router-dom', () => {
     return {
-        useNavigate: () => mockNavegacao
+        useNavigate: () => mockNavigation
     }
-})
+});
 
 describe('when do not have enough participants', () => {
     beforeEach(() => {
         (useListOfParticipants as jest.Mock).mockReturnValue([]);
-    })
+    });
     test('the game can not be started', () => {
         render(<RecoilRoot>
             <Footer />
         </RecoilRoot>);
         const button = screen.getByRole('button');
         expect(button).toBeDisabled();
-    })
-})
+    });
+});
 
 describe('when there are enough participants', () => {
     beforeEach(() => {
         (useListOfParticipants as jest.Mock).mockReturnValue(['Robson', 'Jesus', 'Silva']);
-    })
+    });
     test('the game can be started', () => {
         render(<RecoilRoot>
             <Footer />
         </RecoilRoot>);
         const button = screen.getByRole('button');
         expect(button).not.toBeDisabled();
-    })
+    });
     test('the game was started', () => {
         render(<RecoilRoot>
             <Footer />
@@ -49,7 +49,7 @@ describe('when there are enough participants', () => {
         const button = screen.getByRole('button');
         fireEvent.click(button);
 
-        expect(mockNavegacao).toHaveBeenCalledTimes(1);
-        expect(mockNavegacao).toHaveBeenCalledWith('/sorteio');
-    })
-})
+        expect(mockNavigation).toHaveBeenCalledTimes(1);
+        expect(mockNavigation).toHaveBeenCalledWith('/sorteio');
+    });
+});
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -14,7 +14,7 @@ const Footer = () => {
     }
 
     return (<footer className="footer-config">
-        <button className="botao" disabled={participants.length < 3} onClick={start}>Iniciar</button>
+        <button className="button" disabled={participants.length < 3} onClick={start}>Iniciar</button>
         <img src="/images/bags.png" alt="Sacolas de compras" />
     </footer>);
 }
