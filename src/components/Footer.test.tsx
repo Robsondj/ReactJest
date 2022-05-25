15b077c1e15f7f2a77f1f7e35b28892ed4dbb39e
Robsondj <robson.djsilva@gmail.com>
2022-05-25 13:30:07 -0300

    🚧 Added footer component

---
+++ src/components/Footer.test.tsx
@@ -0,0 +1,55 @@
+import { fireEvent, render, screen } from "@testing-library/react";
+import React from "react";
+import { RecoilRoot } from "recoil";
+import { useListOfParticipants } from "../state/hook/useListOfParticipants";
+import Footer from "./Footer";
+
+jest.mock('../state/hook/useListOfParticipants', () => {
+    return {
+        useListOfParticipants: jest.fn()
+    }
+})
+
+const mockNavegacao = jest.fn();
+
+jest.mock('react-router-dom', () => {
+    return {
+        useNavigate: () => mockNavegacao
+    }
+})
+
+describe('when do not have enough participants', () => {
+    beforeEach(() => {
+        (useListOfParticipants as jest.Mock).mockReturnValue([]);
+    })
+    test('the game can not be started', () => {
+        render(<RecoilRoot>
+            <Footer />
+        </RecoilRoot>);
+        const button = screen.getByRole('button');
+        expect(button).toBeDisabled();
+    })
+})
+
+describe('when there are enough participants', () => {
+    beforeEach(() => {
+        (useListOfParticipants as jest.Mock).mockReturnValue(['Robson', 'Jesus', 'Silva']);
+    })
+    test('the game can be started', () => {
+        render(<RecoilRoot>
+            <Footer />
+        </RecoilRoot>);
+        const button = screen.getByRole('button');
+        expect(button).not.toBeDisabled();
+    })
+    test('the game was started', () => {
+        render(<RecoilRoot>
+            <Footer />
+        </RecoilRoot>);
+        const button = screen.getByRole('button');
+        fireEvent.click(button);
+
+        expect(mockNavegacao).toHaveBeenCalledTimes(1);
+        expect(mockNavegacao).toHaveBeenCalledWith('/sorteio');
+    })
+})
---
+++ src/components/Footer.tsx
@@ -0,0 +1,22 @@
+import { useNavigate } from "react-router-dom";
+import { useListOfParticipants } from "../state/hook/useListOfParticipants";
+
+import './Footer.css';
+
+const Footer = () => {
+
+    const participants = useListOfParticipants();
+
+    const navigateTo = useNavigate();
+    
+    const start = () => {
+        navigateTo('/sorteio');
+    }
+
+    return (<footer className="footer-config">
+        <button className="botao" disabled={participants.length < 3} onClick={start}>Iniciar</button>
+        <img src="/images/bags.png" alt="Sacolas de compras" />
+    </footer>);
+}
+
+export default Footer;
