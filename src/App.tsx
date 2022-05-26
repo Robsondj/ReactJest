f5e80938773a466835ef424149e1d0de305b6316
Robsondj <robson.djsilva@gmail.com>
2022-05-26 14:43:05 -0300

    🚧 Added draw to routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
-import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { RecoilRoot } from 'recoil';
-import Config from './pages/Config';
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { RecoilRoot } from "recoil";
+import Config from "./pages/Config";
+import Draw from "./pages/Draw";
 
 function App() {
   return (
     <BrowserRouter>
       <RecoilRoot>
         <Routes>
-          <Route path='/' element={Config}/>
+          <Route path="/" element={Config}/>
+          <Route path="/draw" element={<Draw />} />
         </Routes>
       </RecoilRoot>
     </BrowserRouter>
